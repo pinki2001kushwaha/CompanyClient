4676f84266e6308a9f4d16a7fd7e323213892b59
shraddha <kushwahapinki@gmail.com>
2024-11-14 01:29:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -25,7 +25,19 @@ const [description,setDescription]=useState("")
 const [author,setAuthor]=useState("")
 
 const postbooks=async()=>{
+  let data = {
+    title:title,
+    description:description,
+    author:author
+};
+let res = await axios.post("http://localhost:5000", data);
+apiget()
+}
 
+async function deletedata(_id) {
+  let res = await axios.delete(`http://localhost:5000/${_id}`);
+  console.log(res);
+  apiget();
 }
   return (
     <>
@@ -43,6 +55,7 @@ const postbooks=async()=>{
                 <th scope="col" className="px-6 py-3">Title</th>
                 <th scope="col" className="px-6 py-3">Description</th>
                 <th scope="col" className="px-6 py-3">Author</th>
+                <th scope="col" className="px-6 py-3">Action</th>
               </tr>
             </thead>
             <tbody>
@@ -62,6 +75,8 @@ const postbooks=async()=>{
                 <td className="px-6 py-4">{item.title}</td>
                 <td className="px-6 py-4">{item.description}</td>
                 <td className='px-6 py-4'>{item.author}</td>
+                <td className='px-6 py-4'><button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => deletedata(item._id)}
+                >Dark</button></td>
               </tr>
                 
               )
@@ -75,38 +90,41 @@ const postbooks=async()=>{
           <form>
             <div className="mb-4">
               <label htmlFor="productName" className="block text-sm font-medium text-gray-700">
-                Product Name
+              Title
               </label>
               <input
                 type="text"
                 id="productName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm h-12 focus:ring-indigo-500 h-12 focus:border-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm h-12 focus:ring-indigo-500 h-12 focus:border-indigo-500 sm:text-sm" required value={title} onChange={(e)=>setTitle(e.target.value)} 
               />
             </div>
             <div className="mb-4">
               <label htmlFor="color" className="block text-sm font-medium text-gray-700 ">
-                Color
+              Description
               </label>
               <input
                 type="text"
-                id="color"
-
+                id="color" 
+                required
+value={description} onChange={(e)=>setDescription(e.target.value)}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-12  sm:text-sm"
               />
             </div>
             <div className="mb-4">
               <label htmlFor="category" className="block text-sm font-medium text-gray-700">
-                Category
+                Author
               </label>
               <input
                 type="text"
                 id="category"
+                required
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-12"
+                value={author} onChange={(e)=>setAuthor(e.target.value)}
               />
             </div>
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-lg  hover:bg-blue-600 focus:ring-4 focus:ring-blue-300"
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg  hover:bg-blue-600 focus:ring-4 focus:ring-blue-300" onClick={()=>{postbooks()}}
             >
               Submit
             </button>
